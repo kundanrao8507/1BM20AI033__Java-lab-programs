1bb946bfa624e9ce2c8ceaecf2ba2f0c3d349226
kundanrao8507 <81979426+kundanrao8507@users.noreply.github.com>
2022-12-19 10:53:28 +0530

    Create 4.1.java

---
+++ 4/4.1.java
@@ -0,0 +1,24 @@
+import square.*;
+import cube.*;
+import java.lang.Math.random;
+
+abstract class rand
+{
+int number;
+abstract void rand();
+}
+
+class test extends rand
+{
+void rand()
+{
+square a1 = new square();
+for(i=1;i<11;i++)
+{
+number = random(1,101);
+
+int k = 
+}
+}
+
+}
